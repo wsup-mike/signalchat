04ba8e351cf6fc6db7a37e618e1035268ace5d97
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-03 14:58:04 -0500

    Cr8 new state fullName

--- screens/RegisterScreen.jsx
+++ screens/RegisterScreen.jsx
@@ -1,23 +1,25 @@
 import { View, Text, KeyboardAvoidingView, StyleSheet } from 'react-native'
 import { Input, Button } from '@rneui/base'
-import React from 'react'
+import React, { useState } from 'react' // this component will create its own state
 import { StatusBar } from 'expo-status-bar';
 
-
-
 const RegisterScreen = ({ navigation }) => {
-  return (
-    <KeyboardAvoidingView behavior='padding' style={styles.container}> 
-        <StatusBar style='light' />
-      <Text h3 style={{ marginBottom: 50 }}>Create a new Signal Account</Text>
+    
 
-      <View style={styles.inputContainer}>
-        <Input 
-            placeholder='Full Name'
-        />
-      </View>
-    </KeyboardAvoidingView>
-  )
+    return (
+        <KeyboardAvoidingView behavior='padding' style={styles.container}> 
+            <StatusBar style='light' />
+        <Text h3 style={{ marginBottom: 50 }}>Create a new Signal Account</Text>
+
+        <View style={styles.inputContainer}>
+            <Input 
+                placeholder='Full Name' 
+                autoFocus
+                type="text"
+            />
+        </View>
+        </KeyboardAvoidingView>
+    )
 }
 
 export default RegisterScreen
