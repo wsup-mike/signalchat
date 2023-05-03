14e36e78dfa86e717ab42b0d30152b146a1277a8
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-03 08:41:49 -0500

    App.js-initialize Firebase n ready the SDKs

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// Firebase-Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Firebase-TODO: Add SDKs for Firebase products that you want to use
+// Firebase-https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCi3nH7srxwJbmEeo6DQNl2R2m8psPVuQI",
+  authDomain: "signalchat-9ec52.firebaseapp.com",
+  projectId: "signalchat-9ec52",
+  storageBucket: "signalchat-9ec52.appspot.com",
+  messagingSenderId: "818485681140",
+  appId: "1:818485681140:web:9869ab4587c6e2d39d37a1",
+  measurementId: "G-1K3GMHKFTS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function App() {
   return (
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
