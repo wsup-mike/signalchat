4dccd2ab8619cbfdcac0f553f97a5bc9e63f3c95
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-03 10:07:51 -0500

    LoginScrn-Add Status bar, style light

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Button, Input, Image } from '@rneui/base'
+import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
   return (
     <View>
+        <StatusBar style='light' />
       <Text>LoginScreen</Text>
     </View>
   )
