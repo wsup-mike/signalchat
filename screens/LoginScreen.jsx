dbd866595965252591f48071de26121aff7bda6f
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-03 10:36:45 -0500

    Cr8 new state email

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -4,27 +4,29 @@ import { Button, Input, Image } from '@rneui/base'
 import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
-  return (
-    <View>
-        <StatusBar style='light' />
-        <Image 
-            source={require('../assets/signallogo.jpg')}
-            style={{ width: 200, height: 200 }}
-        />
-      <View style={styles.inputContainer}>
-        <Input 
-            placeholder='Email'
-            autoFocus
-            type="email"
-        />
-        <Input 
-            placeholder='Password'
-            type="password"
-            secureTextEntry //hides user input
-        />
-      </View>
-    </View>
-  )
+    const [email, setEmail] = useState('');
+
+    return (
+        <View>
+            <StatusBar style='light' />
+            <Image 
+                source={require('../assets/signallogo.jpg')}
+                style={{ width: 200, height: 200 }}
+            />
+        <View style={styles.inputContainer}>
+            <Input 
+                placeholder='Email'
+                autoFocus
+                type="email"
+            />
+            <Input 
+                placeholder='Password'
+                type="password"
+                secureTextEntry //hides user input
+            />
+        </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
