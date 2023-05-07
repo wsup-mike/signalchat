8875c56e3a6160b74cf24b1853fed31eafd514d6
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-07 09:18:34 -0500

    Cr8 new component CustomListItem

---
+++ components/CustomListItem.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomListItem = () => {
+  return (
+    <View>
+      <Text>CustomListItem</Text>
+    </View>
+  )
+}
+
+export default CustomListItem
