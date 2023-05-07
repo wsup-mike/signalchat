e90c29bdc9278a4d3101bd25cdcd0fda4af953a5
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-07 10:07:37 -0500

    Change title of header with useEffect

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,17 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import CustomListItem from '../components/CustomListItem'
+import { useNavigation } from '@react-navigation/native'
 
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
+
+  const navigation = useNavigation();
 
   useLayoutEffect(() => {
-
+    navigation.setOptions({
+      title: 'Signal'
+    })
   }, [])
 
   return (
