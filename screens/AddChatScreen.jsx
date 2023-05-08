93a2710ad6d682fcd995942a762bd616698c6da7
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-08 12:44:29 -0500

    Add headerLeft with Touchable

--- screens/AddChatScreen.jsx
+++ screens/AddChatScreen.jsx
@@ -1,28 +1,25 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { goBack } from '@react-navigation/native'
 import { Button, Input } from '@rneui/base'
 
 
+
 const AddChatScreen = ({ navigation }) => {
 
-  const BackButton = ({ onPress }) => (
-    <TouchableOpacity onPress={onPress}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <AntDesign name="arrowleft" size={24} color="black" />
-        <Text style={{ marginLeft: 5, fontWeight: 'bold' }}>Chats</Text>
-      </View>
-    </TouchableOpacity>
-  );
-  
-
   useLayoutEffect(() => {
-    navigation.setOptions({
-      title: 'Add a new Chat',
-      headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
-    });
+    navigation.setOptions({ 
+      title: 'Add a new Chat here!',
+      headerLeft: () => {
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <View style={{ marginLeft: 20 }}>
+            <Text>Chats</Text>
+          </View>
+        </TouchableOpacity>
+      },
+    })
   }, [navigation])
-
+  
   return (
     <View style={styles.container}>
       <Text>AddChat</Text>
