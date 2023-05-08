af0c319a666a20b1cc44b9a534d5c52700b014fb
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-08 11:51:54 -0500

    Cr8 new screen AddChat.jsx

---
+++ screens/AddChat.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddChat = () => {
+  return (
+    <View>
+      <Text>AddChat</Text>
+    </View>
+  )
+}
+
+export default AddChat
