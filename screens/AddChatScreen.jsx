a270632756e12e6416bb4039ac18b734974dd13d
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-08 12:30:11 -0500

    Add BackButton for headerLeft

--- screens/AddChatScreen.jsx
+++ screens/AddChatScreen.jsx
@@ -5,11 +5,21 @@ import { Button, Input } from '@rneui/base'
 
 const AddChatScreen = ({ navigation }) => {
 
+  const BackButton = ({ onPress }) => (
+    <TouchableOpacity onPress={onPress}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+        <Text style={{ marginLeft: 5, fontWeight: 'bold' }}>Chats</Text>
+      </View>
+    </TouchableOpacity>
+  );
+  
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Add a new Chat',
-      headerLeft: null,
-      headerBackTitle: "Chats",
+      
+      
     })
   }, [navigation])
 
