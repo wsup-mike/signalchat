1cb125114242827d081187f9f85171ee08498893
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-08 11:33:53 -0500

    import icons AntDesign SimpleLineIcons

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from 'react'
 import CustomListItem from '../components/CustomListItem'
 import { StackActions, useNavigation } from '@react-navigation/native'
 import { Avatar } from '@rneui/base'
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 import { auth } from '../firebase';
 
 
@@ -41,12 +42,11 @@ const HomeScreen = () => {
       headerRight: () => { // We pass a COMPONENT here!
         return (
           <View style={{ marginLeft: 20 }}> 
-            <TouchableOpacity onPress={signOut}>
-              <Avatar 
-                rounded
-                source={{ uri: auth?.currentUser?.photoURL }} // if both user authenticated and photoURL submitted
-                // source={{ uri: 'https://picsum.photos/200/300.jpg'}}
-                size={'small'}
+            <TouchableOpacity>
+              <AntDesign 
+                name='camerao'
+                size={24}
+                color='black'
               />
             </TouchableOpacity>
           </View>
