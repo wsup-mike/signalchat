c0b608b169cd1fb2534878386728d1e209b85579
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-08 10:25:43 -0500

    import TouchableOpac

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import CustomListItem from '../components/CustomListItem'
 import { useNavigation } from '@react-navigation/native'
@@ -18,12 +18,14 @@ const HomeScreen = () => {
       headerTintColor: "black", //make any image logos here black
       headerLeft: () => { // We pass a COMPONENT here!
         <View style={{ marginLeft: 20 }}> 
-          <Avatar 
-            rounded
-            source={{ uri: auth?.currentUser?.photoURL }} // if both user authenticated and photoURL submitted
-            // source={{ uri: 'https://picsum.photos/200/300.jpg'}}
-            size={'small'}
-          />
+          <TouchableOpacity>
+            <Avatar 
+              rounded
+              source={{ uri: auth?.currentUser?.photoURL }} // if both user authenticated and photoURL submitted
+              // source={{ uri: 'https://picsum.photos/200/300.jpg'}}
+              size={'small'}
+            />
+          </TouchableOpacity>
         </View>
       },
     })
