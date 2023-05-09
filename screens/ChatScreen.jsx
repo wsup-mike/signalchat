f3588c663553c29990aa7c443a03fff3b20c83fe
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 16:50:54 -0500

    ChatScrn-setup useLayoutEffect

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLayoutEffect } from 'react';
 
 const ChatScreen = ({ navigation, route }) => {
-  return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
+    useLayoutEffect(() => {
+        
+    }, [])
+
+    return (
+        <View>
+        <Text>{route.params.chatName}</Text>
+        </View>
+    )
 }
 
 export default ChatScreen
