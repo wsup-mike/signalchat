c9a1dc9bc82ebebbfd97c7fa7325fee656b1ae74
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 16:39:26 -0500

    App-import ChatScrn

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import AddChatScreen from "./screens/AddChatScreen";
+import ChatScreen from "./screens/ChatScreen";
 
 const Stack = createNativeStackNavigator();
 
