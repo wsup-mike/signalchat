cf9cfd051db36d79fc6d91a60e938f147c2b05d0
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 16:48:49 -0500

    ChatScrn-destructure navigation, route and pass in

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
   return (
     <View>
       <Text>ChatScreen</Text>
