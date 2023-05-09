5f0ac98e465c08ae56e22037f37f20060eb8a0bc
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 16:44:53 -0500

    Add enterChat attribute to <CustomlistItem>

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -108,7 +108,7 @@ const HomeScreen = () => {
             key={id}
             id={id}
             chatName={chatName}
-            
+            enterChat={enterChat}
           />
         ))}
       </ScrollView>
