7c2a4f74e2f9c42a32fa5c722df8911363d1a753
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 21:51:51 -0500

    Set up sendMessage function signature

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -55,6 +55,10 @@ const ChatScreen = ({ navigation, route }) => {
         })
     }, [navigation])
 
+    const sendMessage = () => {
+        
+    }
+
     return (
         <SafeAreaView style={{
             flex: 1,
