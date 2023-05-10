355f7a306dc45a2894c7eaa86e43be27d1a970eb
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-10 14:46:50 -0500

    import orderBy query

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -7,7 +7,7 @@ import { Avatar } from '@rneui/base';
 import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons'
 import { StatusBar } from 'expo-status-bar';
 import { db, auth } from '../firebase'
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp, query, orderBy } from 'firebase/firestore';
 import firebase from 'firebase/app'
 
 
