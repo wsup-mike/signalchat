ee609a91048865c2c7f85452b1c29b50c54e6779
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 20:52:48 -0500

    import Avatar

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLayoutEffect } from 'react';
+import { Avatar } from '@rneui/base';
 
 const ChatScreen = ({ navigation, route }) => {
     useLayoutEffect(() => {
