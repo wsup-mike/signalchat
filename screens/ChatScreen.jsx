3450e6bb04e26a887c291de2975e9c871c3e0507
Mike Ramos <michaelangeloram8@gmail.com>
2023-05-09 21:51:07 -0500

    Add 'Send Message' button with sendMessage functio

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -77,6 +77,9 @@ const ChatScreen = ({ navigation, route }) => {
                             value={textInput}
                             onChangeText={text => setTextInput(text)}
                         />
+                        <TouchableOpacity onPress={sendMessage}>
+
+                        </TouchableOpacity>
                     </View>
                 </>
             </KeyboardAvoidingView>
